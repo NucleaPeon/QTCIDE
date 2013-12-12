c1150e79813de8a291b09b463ffaadd8365505d1
Daniel Kettle <initial.dann@gmail.com>
2013-12-11 17:16:54 -0800

    Terminal accepts string inputs, clears when Enter/Return is pressed

--- src/view/docks/terminal.py
+++ src/view/docks/terminal.py
@@ -3,6 +3,8 @@
 from PyQt4 import QtGui, QtCore
 
 class TerminalDock(QtGui.QWidget):
+
+    __version__ = "v 0.10"
     
     def __init__(self, *args, **kwargs):
         '''
@@ -19,10 +21,14 @@ class TerminalDock(QtGui.QWidget):
         pal = self.textedit.palette()
         pal.setColor(QtGui.QPalette.Base, QtCore.Qt.black)
         self.textedit.setPalette(pal)
+        self.connect(self.textedit,
+            QtCore.SIGNAL("keyPressEvent(const QKeyEvent &)"),
+            self.keyPressEvent)
         self.textedit.setTextColor(QtGui.QColor(QtCore.Qt.white))
         self.textedit.resize(width - 24, 100 - 24)
         self.textedit.setReadOnly(True)
-        self.textedit.setText("Terminal Loaded: QTCIDE [V 0.10]")
+        self.textedit.setText(
+            "> QTCIDE [{}]".format(self.__version__))
         self.consolefont = QtGui.QFont()
         self.consolefont.setStyleHint(QtGui.QFont.Monospace)
         self.textedit.setCurrentFont(self.consolefont)
@@ -31,3 +37,11 @@ class TerminalDock(QtGui.QWidget):
         vbox.addWidget(scrollarea)
         vbox.addWidget(self.terminalinput)
         scrollarea.adjustSize()
+
+    @QtCore.pyqtSlot(QtGui.QKeyEvent)
+    def keyPressEvent(self, e):
+        if self.terminalinput.text() == "":
+            return
+        if e.key() == QtCore.Qt.Key_Enter or e.key() == QtCore.Qt.Key_Return:
+            self.textedit.append("> {}".format(self.terminalinput.text()))
+            self.terminalinput.clear()
