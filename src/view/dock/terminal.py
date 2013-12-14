3e5c96dee780bc87a658c95dd81d95a62086a653
Daniel Kettle <initial.dann@gmail.com>
2013-12-13 18:50:40 -0700

    Terminal works but without white on black theme as prior one

--- src/view/dock/terminal.py
+++ src/view/dock/terminal.py
@@ -9,3 +9,34 @@ class Terminal(QtGui.QDockWidget):
         self.setWindowTitle(QtGui.QApplication.translate(
             "self", "Terminal", None, QtGui.QApplication.UnicodeUTF8))
         self.setWidget(self.widget)
+        self.vbox = QtGui.QVBoxLayout()
+        self.widget.setLayout(self.vbox)
+        # Monospace Font
+        self.consolefont = QtGui.QFont()
+        self.consolefont.setStyleHint(QtGui.QFont.Monospace)
+        # scroll area settings
+        self.scrollarea = QtGui.QScrollArea()
+        self.scrollarea.setWidgetResizable(True)
+        # line edit settings
+        self.commandarea = QtGui.QLineEdit()
+        # terminal output area
+        self.textedit = QtGui.QTextEdit()
+        self.textedit.setCurrentFont(self.consolefont)
+        self.textedit.setReadOnly(True)
+        self.textedit.setText("")
+        # Add Widgets to Layout
+        self.scrollarea.setWidget(self.textedit)
+        self.vbox.addWidget(self.scrollarea)
+        self.vbox.addWidget(self.commandarea)
+        # Connect slots
+        self.connect(self.textedit,
+                     QtCore.SIGNAL("keyPressEvent(const QKeyEvent &)"),
+                     self.keyPressEvent)
+        
+    @QtCore.pyqtSlot(QtGui.QKeyEvent)    
+    def keyPressEvent(self, e):
+        if self.commandarea.text() == "":
+            return
+        if e.key() == QtCore.Qt.Key_Enter or e.key() == QtCore.Qt.Key_Return:
+            self.textedit.append(": {}".format(self.commandarea.text()))
+            self.commandarea.clear()
