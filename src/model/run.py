5e1847ea3aa8ac5c7b87146ba618b8141ea90924
Daniel Kettle <initial.dann@gmail.com>
2013-12-24 07:23:14 -0700

    Refactored model to use Run instead of Compiler

--- src/model/run.py
+++ src/model/run.py
@@ -1,27 +1,27 @@
 from PyQt4 import QtGui, QtCore
 from view.img import SYS_IMG_FOLDER, SYS_APP_ICON
 import view.actions.project.build.build as build
-import view.menu.compilercontext
+import view.menu.runcontext
 import os
 
-class Compiler:
+class Run:
     
     _instance = None # Single instance of initialized class
     
     def __new__(self,  *args, **kwargs):
         if not self._instance:
-            self._instance = super(Compiler, self).__new__(
+            self._instance = super(Run, self).__new__(
                 self, *args, **kwargs)
-            self.compilers = QtGui.QStandardItemModel() # Project data
-            self.compilers.setHorizontalHeaderItem(0, 
-                                                  QtGui.QStandardItem("Compiler"))
-            self.rootNode = self.compilers.invisibleRootItem()
+            self.runs = QtGui.QStandardItemModel() # Project data
+            self.runs.setHorizontalHeaderItem(0, 
+                                                  QtGui.QStandardItem("Run"))
+            self.rootNode = self.runs.invisibleRootItem()
             self.qicon = QtGui.QIcon(os.path.join(SYS_IMG_FOLDER, 'configure.png'))
             test = QtGui.QStandardItem(build.BuildSystemBuildAction().qicon, "gcc")
             test.setEditable(False)
             test.appendRow(QtGui.QStandardItem(self.qicon, "Test Build Configuration"))
             self.rootNode.appendRow(test)
-            self.compilertree = QtGui.QTreeView()
-            self.compilercontextmenu = view.menu.compilercontext.CompilerContextMenu()
-            self.compilertree.setModel(self.compilers)
+            self.runtree = QtGui.QTreeView()
+            self.runcontextmenu = view.menu.runcontext.RunContextMenu()
+            self.runtree.setModel(self.runs)
         return self._instance
