b9b9287b7d79a8d2bbc552dd6db0dc2adacf6f33
Daniel Kettle <initial.dann@gmail.com>
2014-01-17 23:04:01 -0700

    Removed old files, added new module for center widget in ide window

---
+++ src/view/main/widget.py
@@ -0,0 +1,8 @@
+from PyQt4 import QtGui, QtCore
+
+'''
+Integrated Shell is a widget that contains other widgets that act
+on the desired programming language for drag and drop functionality.
+'''
+class IntegratedShell(QtGui.QWidget):
+    pass
